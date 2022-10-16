13e8083a774d8fd9052db432de518dabbb673f92
Pedro Remoaldo <premoaldo@gmail.com>
2022-10-16 20:40:51 +0100

    My second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Person person1 = new Person("Pedro Remoaldo", "Portugal");
+
+        System.out.println(person1.getName());
     }
 }
